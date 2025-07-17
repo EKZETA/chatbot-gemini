54c24145a5d32d01f775a65a0d447b84d9925d9b
Ekzo Vincenzo <ekopriambodo12gt@gmail.com>
2025-07-17 21:51:41 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+import google.generativeai as genai
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+try:
+    genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    print("Model berhasil dikonfigurasi")
+except AttributeError as e:
+    print(f"Error: {e}. Pastikan Anda telah mengatur variabel lingkungan dengan benar.")
+    exit()
+
+@app.route('/chat', methos=["post"])
+def chat():
