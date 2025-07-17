4fc5693049aa28038bb6bb63cf3360e7153b56b2
Ekzo Vincenzo <ekopriambodo12gt@gmail.com>
2025-07-17 22:01:18 +0700

    C:/Program Files/Git/chat function and bot response

--- app.py
+++ app.py
@@ -17,3 +17,18 @@ except AttributeError as e:
 
 @app.route('/chat', methos=["post"])
 def chat():
+    user_message = request.json.get("message")
+
+    if not user_message:
+        return jsonify({"error":"Pesan tidak boleh kosong"}), 400
+    
+    try:
+        response = model.generate_content(user_message)
+        bot_response = response.text
+        return jsonify({"reply": bot_response})
+    
+    except Exception as e:
+        return jsonify({"error":str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
